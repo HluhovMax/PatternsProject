ebde2cb619dcf8d930cf04023944073bc5df4086
Max Hluhov <holosievo@gmail.com>
2018-11-06 19:54:05 +0200

    added proxy package

---
+++ src/main/java/patterns/structural/proxy/ThirdPartyYoutubeLib.java
@@ -0,0 +1,9 @@
+package patterns.structural.proxy;
+
+import java.util.HashMap;
+
+public interface ThirdPartyYoutubeLib {
+    public HashMap<String, Video> popularVideos();
+
+    public Video getVideo(String videoId);
+}
