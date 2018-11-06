a50555f55f0e5b3bc3ae9ab1fb68b796666e71aa
Max Hluhov <holosievo@gmail.com>
2018-11-06 16:40:21 +0200

    added facade package

---
+++ src/main/java/patterns/structural/facade/OggCompressionCodec.java
@@ -0,0 +1,4 @@
+package patterns.structural.facade;
+public class OggCompressionCodec implements Codec {
+    public String type = "ogg";
+}
---
+++ src/main/java/patterns/structural/facade/VideoFile.java
@@ -0,0 +1,18 @@
+package patterns.structural.facade;
+public class VideoFile {
+    private String name;
+    private String codecType;
+
+    public VideoFile(String name) {
+        this.name = name;
+        this.codecType = name.substring(name.indexOf(".") + 1, name.length());
+    }
+
+    public String getCodecType() {
+        return codecType;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
